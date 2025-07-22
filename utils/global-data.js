abf973ce1c3d1ae34fccdfe92e574a736179c560
huqiusheng2580 <3314385581@qq.com>
2025-07-22 17:16:22 +0800

    Refactor code structure for improved readability and maintainability

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'qiuren';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'qiuren 的博客';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : '保留所有权利。';
 
   return {
     name,
